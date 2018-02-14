56e7c901f4e841eed2a4dca41df6cdf9e8f47ad8
oranthony <anthonyloroscio@gmail.com>
2018-02-14 23:18:52 +0100

    Client : map component display user position

--- client/src/app/map-page/map-page.component.ts
+++ client/src/app/map-page/map-page.component.ts
@@ -3,7 +3,8 @@ import * as mapboxgl from 'mapbox-gl';
 import { MapService } from '../map.service';
 import { GeoJson, FeatureCollection } from '../map';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Accident from "../models/accident.model";
+import Accident from '../models/accident.model';
+import {LngLat, Map} from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-page',
@@ -12,38 +13,71 @@ import Accident from "../models/accident.model";
 })
 export class MapPageComponent implements OnInit {
 
+    public geojson = {
+        type: 'FeatureCollection',
+        features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.032, 38.913]
+            },
+            properties: {
+                title: 'Mapbox',
+                description: 'Washington, D.C.'
+            }
+        },
+            {
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [-122.414, 37.776]
+                },
+                properties: {
+                    title: 'Mapbox',
+                    description: 'San Francisco, California'
+                }
+            }]
+    };
+
     @Input() accidentsList: Accident[];
 
-    map: mapboxgl.Map;
-    style = 'mapbox://styles/mapbox/outdoors-v9';
+    map: Map;
+
+    // map: mapboxgl.Map;
+    // style = 'mapbox://styles/mapbox/outdoors-v9';
+    style = 'mapbox://styles/mapbox/light-v9';
     lat = 48.86;
     lng = 2.33;
     message = '';
     // data
     source: any;
     markers: any;
+
     constructor(private mapService: MapService) {
     }
+
     ngOnInit() {
-        // this.markers = this.mapService.getMarkers()
         this.initializeMap();
     }
+
     private initializeMap() {
         /// locate the user
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
                 this.lat = position.coords.latitude;
                 this.lng = position.coords.longitude;
+
                 this.map.flyTo({
                     center: [this.lng, this.lat]
                 });
+
             });
         }
         this.buildMap();
     }
     buildMap() {
 
-        this.map = new mapboxgl.Map({
+        this.map = new Map({
             container: 'map',
             style: this.style,
             zoom: 13,
@@ -54,57 +88,73 @@ export class MapPageComponent implements OnInit {
         const nav = new mapboxgl.NavigationControl();
         this.map.addControl(nav, 'bottom-left');
 
-        //// Add Marker on Click
-        this.map.on('click', (event) => {
-            const coordinates = [event.lngLat.lng, event.lngLat.lat];
-            const newMarker   = new GeoJson(coordinates, { message: this.message });
-            // this.mapService.createMarker(newMarker)
-        });
+        // Add geolocate control to the map.
+        this.map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        }));
+
+        // Show user location
+        //
 
         /// Add realtime firebase data on map load
         this.map.on('load', (event) => {
-            /// register source
-            this.map.addSource('firebase', {
-                type: 'geojson',
-                data: {
-                    type: 'FeatureCollection',
-                    features: []
-                }
-            });
-            /// get source
-            this.source = this.map.getSource('firebase');
-            /// subscribe to realtime database and set data source
-            /*this.markers.subscribe(markers => {
-                let data = new FeatureCollection(markers)
-                this.source.setData(data)
-            })*/
-            /// create map layers with realtime data
-            this.map.addLayer({
-                id: 'firebase',
-                source: 'firebase',
-                type: 'symbol',
-                layout: {
-                    'text-field': '{message}',
-                    'text-size': 24,
-                    'text-transform': 'uppercase',
-                    'icon-image': 'rocket-15',
-                    'text-offset': [0, 1.5]
-                },
-                paint: {
-                    'text-color': '#f16624',
-                    'text-halo-color': '#fff',
-                    'text-halo-width': 2
-                }
-            });
+
+           this.showMarkers();
         });
     }
+
     /// Helpers
     removeMarker(marker) {
         // this.mapService.removeMarker(marker.$key)
     }
+
     flyTo(data: GeoJson) {
         this.map.flyTo({
             center: data.geometry.coordinates
+        });
+    }
+
+    showMarkers() {
+        this.map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': [{
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [this.lng, this.lat]
+                        },
+                        'properties': {
+                            'title': 'Mapbox DC',
+                            'icon': 'monument'
+                        }
+                    }, {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [-122.414, 37.776]
+                        },
+                        'properties': {
+                            'title': 'Mapbox SF',
+                            'icon': 'harbor'
+                        }
+                    }]
+                }
+            },
+            'layout': {
+                'icon-image': '{icon}-15',
+                'text-field': '{title}',
+                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                'text-offset': [0, 0.6],
+                'text-anchor': 'top'
+            }
         });
     }
 }
