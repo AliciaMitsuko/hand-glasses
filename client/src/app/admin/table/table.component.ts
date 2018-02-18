0902035486cd8af6d1a18b5c55d8ad86928bfc3f
Alicia Marin <ma202240@etu.unice.fr>
2018-02-18 18:31:32 +0100

    getAccidentById

--- client/src/app/admin/table/table.component.ts
+++ client/src/app/admin/table/table.component.ts
@@ -33,78 +33,22 @@ export class TableComponent implements OnInit {
 
   atmToIcon(num: number) {
     return this.iconService.atmToIcon(num);
-
-    // console.log(num)
-    // switch(num) {
-    //   case 1: {
-    //     return '../../../assets/icons/sun.svg'
-    //   }
-    //   case 2 || 3: {
-    //     return '../../../assets/icons/rain.svg'
-    //   }
-    //   case 4: {
-    //     return '../../../assets/icons/snowflake.svg'
-    //   }
-    //   case 5: {
-    //     return '../../../assets/icons/cloud.svg'
-    //   }
-    //   case 6: {
-    //     return '../../../assets/icons/wind.svg'
-    //   }
-    //   case 7: {
-    //     return '../../../assets/icons/sun.svg'
-    //   }
-    //   case 8: {
-    //     return '../../../assets/icons/cloud.svg'
-    //   }
-    //   default: {
-    //     return '../../../assets/icons/question.svg'
-    //
-    //   }
-    // }
   }
 
   lumToIcon(num: number) {
     return this.iconService.lumToIcon(num);
-
-    // switch(num) {
-    //   case 1: {
-    //     return '../../../assets/icons/sun.svg'
-    //   }
-    //   case 2: {
-    //     return '../../../assets/icons/twilight.svg'
-    //   }
-    //   case 3  || 4 || 5 : {
-    //     return '../../../assets/icons/moon.svg'
-    //   }
-    //   default: {
-    //     return '../../../assets/icons/question.svg'
-    //
-    //   }
-    // }
   }
 
   gravToIcon(num: number) {
     return this.iconService.gravToIcon(num);
-    // switch(num) {
-    //   case 1: {
-    //     return '../../../assets/icons/like.svg'
-    //   }
-    //   case 2: {
-    //     return '../../../assets/icons/skull.svg'
-    //   }
-    //   case 3  || 4 : {
-    //     return '../../../assets/icons/accident.svg'
-    //   }
-    //   default: {
-    //     return '../../../assets/icons/question.svg'
-    //
-    //   }
-    // }
   }
 
   surfToIcon(num: number) {
     return this.iconService.surfToIcon(num);
+  }
+
+  setAccidentToEdit(accident: Accident) {
+    this.dataService.changeAccidentToEdit(accident);
   }
 
   public toInt(num: string) {
--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -5,15 +5,26 @@ import Accident from "../models/accident.model";
 @Injectable()
 export class DataService {
 
+  // accidentList loaded in admin-page
   private accidentsListSource = new BehaviorSubject<Accident[]>([]);
   accidentsList = this.accidentsListSource.asObservable();
+
+  // accident loaded in the modal
+  private accidentToEditSource = new BehaviorSubject<Accident>(new Accident());
+  accidentToEdit = this.accidentToEditSource.asObservable();
 
 
   constructor() { }
 
+  // accidentList loaded in admin-page
   changeAccidentList(message: Accident[]) {
     this.accidentsListSource.next(message)
   }
 
+
+  // accident loaded in the modal
+  changeAccidentToEdit(message: Accident) {
+    this.accidentToEditSource.next(message)
+  }
 
 }
