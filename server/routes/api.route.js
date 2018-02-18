c2c6090ef595e754266e8fa4cefa76e81f8181ca
Unknown <nicobighetti@gmail.com>
2018-02-18 23:58:42 +0100

    delete todo related files

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,12 +1,9 @@
 var express = require('express')
 
 var router = express.Router()
-var todos = require('./api/todos.route')
 var accidents = require('./api/accidents.route')
 var dangers = require('./api/dangers.route');
 
-
-router.use('/todos', todos);
 router.use('/accidents', accidents);
 router.use('/dangers', dangers);
 
