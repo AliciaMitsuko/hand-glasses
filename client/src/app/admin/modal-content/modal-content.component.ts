867920aaeebd07fd158dc2be1e97135d825dd8de
Alicia Marin <ma202240@etu.unice.fr>
2018-02-19 11:18:12 +0100

    map on modal

--- client/src/app/admin/modal-content/modal-content.component.ts
+++ client/src/app/admin/modal-content/modal-content.component.ts
@@ -7,6 +7,10 @@ import Accident from "../../models/accident.model";
 import {AccidentService} from "../../services/accident.service";
 import {Observable} from "rxjs/Observable";
 import {IconService} from "../../services/icon.service";
+import {environment} from '../../../environments/environment';
+import * as mapboxgl from 'mapbox-gl';
+import {LngLat, Map} from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 @Component({
   selector: 'app-modal-content',
@@ -22,8 +26,15 @@ export class ModalContentComponent implements OnInit {
   countSurf:Observable<number[]>;
   countGrav:Observable<number[]>;
 
+  boolMarker = true;
+
   accidentToEdit: Accident;
   accidentsList: Accident[];
+
+  private lat = 48.8566;
+  private lng = 2.3522;
+  public map: Map;
+  private style = 'mapbox://styles/mapbox/light-v9';
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -39,8 +50,87 @@ export class ModalContentComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.dataService.accidentToEdit.subscribe(message => this.accidentToEdit = message)
+
+    this.buildMap();
+
+    this.dataService.accidentToEdit.subscribe(message => {
+      this.accidentToEdit = message
+      if (message['geojson'] != undefined) {
+        this.lng = message['geojson'].coordinates[0].valueOf();
+        this.lat = message['geojson'].coordinates[1].valueOf();
+
+
+        this.map.on('load', (event) => {
+          if (this.boolMarker) {
+            this.showMarker();
+            this.boolMarker = false; // temporaty fixe
+          }
+
+          this.map.flyTo({
+            center: [this.lng, this.lat]
+          });
+        });
+
+      }
+    });
     this.dataService.accidentsList.subscribe(message => this.accidentsList = message) // moche :(
+
+  }
+
+  buildMap() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+    this.map = new Map({
+      container: 'map',
+      style: this.style,
+      zoom: 8,
+      center: [this.lng, this.lat]
+    });
+    this.boolMarker = true;
+
+
+    /*this.map.on('load', (event) => {
+      this.showMarker();
+
+      this.map.flyTo({
+        center: [this.lng, this.lat]
+      });
+    });*/
+
+
+    /// Add map controls
+    // const nav = new mapboxgl.NavigationControl();
+    // this.map.addControl(nav, 'bottom-left');
+
+    // Add geolocate control to the map.
+    // this.map.addControl(new mapboxgl.GeolocateControl({
+    //   positionOptions: {
+    //     enableHighAccuracy: true
+    //   },
+    //   trackUserLocation: true
+    // }));
+  }
+
+  showMarker() {
+
+    this.map.addLayer({
+      'id': this.accidentToEdit._id,
+      'type': 'circle',
+      'source': {
+        'type': 'geojson',
+        'data': {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [this.lng, this.lat]
+            }
+          }]
+        }
+      }
+    });
+
+
   }
 
   close() {
