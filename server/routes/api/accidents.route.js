467889e113c2de43fe6a171ecb537731c04708f7
Thibaut Terris <thibaut.terris@gmail.com>
2018-02-19 20:07:13 +0100

    Add route to all preprocessing function

--- server/routes/api/accidents.route.js
+++ server/routes/api/accidents.route.js
@@ -3,7 +3,12 @@ var express = require('express')
 var router = express.Router()
 
 var AccidentController = require('../../controllers/accidents.controller');
+var PreprocessingController = require('../../controllers/PreprocessingData.controller');
 
+router.get('/', AccidentController.getAccidents)
+router.get('/preprocessingDep', PreprocessingController.transformRegionCode)
+router.get('/preprocessingCom', PreprocessingController.transformComCodeToPostalCode)
+router.get('/checkPostCode', PreprocessingController.checkPostCode)
 
 router.get('/id/:id', AccidentController.getAccidentsById)
 router.get('/id/:id/:format', AccidentController.getAccidentsById)
