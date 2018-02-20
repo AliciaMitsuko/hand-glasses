a8bc145ddad857744fd2108a12854aafc327d33f
Thibaut Terris <thibaut.terris@gmail.com>
2018-02-20 15:01:02 +0100

    Add new route to authentification

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,11 +1,13 @@
 var express = require('express')
 
 var router = express.Router()
-var accidents = require('./api/accidents.route')
+var accidents = require('./api/accidents.route');
 var dangers = require('./api/dangers.route');
+var authentification = require('./api/authentification.route');
 
 router.use('/accidents', accidents);
 router.use('/dangers', dangers);
+router.use('/authentification', authentification);
 
 
 module.exports = router;
