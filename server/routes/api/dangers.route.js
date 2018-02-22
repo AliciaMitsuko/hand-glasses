fa0fd3e0b58aa3d313907fb84e489528d5dbb0bd
Unknown <nicobighetti@gmail.com>
2018-02-22 09:12:59 +0100

    /!\ FIX missing data in swagger description

--- server/routes/api/dangers.route.js
+++ server/routes/api/dangers.route.js
@@ -12,8 +12,34 @@ router.post('/crossed/:format', DangerController.getCrossedDangers);
 router.get('/', DangerController.getNearbyDangers).describe({
     summary: "Récupère les accidents proche géographiquement",
     description: "Récupère les accidents distant d'au plus n mètres en fonction de la latitude et de la longitude",
+    tags: [
+      "Dangers"
+    ],
     operationId: "Nearby",
     produces:["application/json"],
+    parameters:[
+    {
+      "in":"query",
+      "name":"lat",
+      "type" : "number",
+      "required": true,
+      "description":"La latitude désirée"
+    },
+    {
+      "in":"query",
+      "name":"long",
+      "type" : "number",
+      "required": true,
+      "description":"La longitude désirée"
+    },
+    {
+      "in":"query",
+      "name":"distance",
+      "type" : "number",
+      "required": true,
+      "description":"La distance de recherche voulue en mètres"
+    }
+],
     responses: {
         200: {
             description: "Return a list of nearby accidents"
